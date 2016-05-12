cd8d99e04214f1341cf7973900045bf6444b72bb
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-12 23:29:33 +0530

    Refactored code

--- install.sh
+++ install.sh
@@ -5,53 +5,51 @@ source variables.sh
 source functions.sh
 
 # Remove all the old files/folders
-echo warn "Removing old files/folders"
-requireRoot rm -rfv /tools
-requireRoot rm -rfv /cross-tools
-requireRoot rm -rfv "${PWD}/.config"
+echo warn "Removing old folders"
+requireRoot rm -rf /tools
+requireRoot rm -rf /cross-tools
 echo success "Finished..."
 echo empty
 
-
-# Create new configuration file
-cat > "${PWD}/.config" << "EOF"
-#!/usr/bin/env bash
-EOF
-
-# Update installation configuration information
-echo norm "export INSTALL_DIR=${INSTALL_DIR}"                                        >> "${PWD}/.config"
-echo norm "export MAKE_TESTS=TRUE"                                                   >> "${PWD}/.config"
-echo norm "export MAKE_PARALLEL="-j$(cat /proc/cpuinfo | grep processor | wc -l)""   >> "${PWD}/.config"
-echo norm "export TARGET=$TARGET" 		                                            >> "${PWD}/.config"
-echo norm "export PATH=$TMP_PATH"		                                            >> "${PWD}/.config"
-echo norm "export HOST=$HOST"     		                                            >> "${PWD}/.config"
-echo norm "export BUILD64=$BUILD64"  	                                            >> "${PWD}/.config"
-echo norm "export LC_ALL=$LC_ALL" 		                                            >> "${PWD}/.config"
-echo norm "export VM_LINUZ=$VM_LINUZ" 		                                        >> "${PWD}/.config"
-echo norm "export SYSTEM_MAP=$SYSTEM_MAP"                                        	>> "${PWD}/.config"
-echo norm "export CONFIG_BACKUP=$CONFIG_BACKUP"                                      >> "${PWD}/.config"
-
-# Make all the configurations available
-source "${PWD}/.config"
-
-# Create necessary directories and symlinks
-echo warn "Creating necessary folders. Please wait..."
-requireRoot install -dv "${TOOLS_DIR}"
-requireRoot ln -sv "${TOOLS_DIR}" /
-requireRoot install -dv "${CROSS_TOOLS_DIR}"
-requireRoot ln -sv "${CROSS_TOOLS_DIR}" /
-echo success "Finished..."
-echo empty
+# Create installation folder
+requireRoot mkdir -p "${INSTALL_DIR}"
 
 # Change folder permissions to `whoami`
 requireRoot chown -R `whoami` "${INSTALL_DIR}"
 
-# Copy the data to the installation directory
+# Create new configuration file
+cat > "${INSTALL_DIR}/.config" << "EOF"
+#!/usr/bin/env bash
+EOF
+
+# Update installation configuration information
+echo norm "export INSTALL_DIR=${INSTALL_DIR}"                                       >> "${INSTALL_DIR}/.config"
+echo norm "export MAKE_TESTS=TRUE"                                                  >> "${INSTALL_DIR}/.config"
+echo norm "export MAKE_PARALLEL="-j$(cat /proc/cpuinfo | grep processor | wc -l)""  >> "${INSTALL_DIR}/.config"
+echo norm "export TARGET=$TARGET" 		                                            >> "${INSTALL_DIR}/.config"
+echo norm "export PATH=$TMP_PATH"		                                            >> "${INSTALL_DIR}/.config"
+echo norm "export HOST=$HOST"     		                                            >> "${INSTALL_DIR}/.config"
+echo norm "export BUILD64=$BUILD64"  	                                            >> "${INSTALL_DIR}/.config"
+echo norm "export LC_ALL=$LC_ALL" 		                                            >> "${INSTALL_DIR}/.config"
+echo norm "export VM_LINUZ=$VM_LINUZ" 		                                        >> "${INSTALL_DIR}/.config"
+echo norm "export SYSTEM_MAP=$SYSTEM_MAP"                                        	>> "${INSTALL_DIR}/.config"
+echo norm "export CONFIG_BACKUP=$CONFIG_BACKUP"                                     >> "${INSTALL_DIR}/.config"
+
+# Make all the configurations available
+source "${INSTALL_DIR}/.config"
+
+# Create necessary directories and symlinks
+echo warn "Creating necessary folders. Please wait..."
+requireRoot install -d "${TOOLS_DIR}"
+requireRoot ln -s "${TOOLS_DIR}" /
+requireRoot install -d "${CROSS_TOOLS_DIR}"
+requireRoot ln -s "${CROSS_TOOLS_DIR}" /
+echo success "Finished..."
 echo empty
+
+# Copy the data to the installation directory
 echo warn "Copying data to ${INSTALL_DIR}. Please wait..."
 cp -ur ./* "${INSTALL_DIR}"
-cp -ur "${PWD}/.config" "${INSTALL_DIR}"
-echo success "Finished copying..."
 echo empty
 
 #----------------------------------------------------------------------------------------------------#
