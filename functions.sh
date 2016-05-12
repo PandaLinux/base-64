37d61b204bfb41f4b3b6dace8dcd63c32a0bd89f
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-12 17:04:50 +0530

    Added new echo function
    This function colorizes the echo command

--- functions.sh
+++ functions.sh
@@ -2,18 +2,36 @@
 
 ## This file contains all the function that will be used throughout the build process
 
-source variables.sh
-
 function requireRoot() {
     # Already root?
     if [[ `whoami` == 'root' ]]; then
+        echo "${BOLD}S*${NORM}"
         $*
     elif [[ -x /bin/sudo || -x /usr/bin/sudo ]]; then
         echo "${BOLD}sudo $*${NORM}"
         sudo $*
     else
-        echo "${REV}We require root privileges to install.${NORM}"
-        echo "${REV}Please run the script as root.${NORM}"
+        echos error "${REV}We require root privileges to install.${NORM}"
+        echos error "${REV}Please run the script as root.${NORM}"
         exit 1
     fi
+}
+
+function echos() {
+    case $1 in
+        success )
+            echo "${GREEN}$2${NORM}";;
+
+        warn )
+            echo "${WARN}$2${NORM}";;
+
+        error )
+            echo "${RED}$2${NORM}";;
+
+        bold )
+            echo "${BOLD}$2${NORM}";;
+
+        empty )
+            echo "";;
+    esac
 }
