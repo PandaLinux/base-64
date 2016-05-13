edd35f767e95609d8ed1cb75758c4621c12a5866
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-14 01:36:41 +0530

    Added bzip2 package

---
+++ temp-system/bzip2/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="bzip2"
+PKG_VERSION="1.0.6"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Bzip2 package contains programs for compressing and decompressing files. Compressing text"
+    echo -e "files with bzip2 yields a much better compression percentage than with the traditional gzip."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "../../sources/$TARBALL" "$TARBALL"
+    source "${CONFIG_FILE}"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    cp -v Makefile{,.orig}
+    sed -e 's@^\(all:.*\) test@\1@g' Makefile.orig > Makefile
+
+    make CC="${CC}" AR="${AR}" RANLIB="${RANLIB}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make PREFIX="${HOST_TOOLS_DIR}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "${HOST_TOOLS_DIR}/bin/bzip2" ]; then
+    touch DONE
+fi
