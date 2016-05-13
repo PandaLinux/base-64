0d8a19953ddb7e4397e09f91cc68264fa4cb2346
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-14 02:10:43 +0530

    Added coreutils package

---
+++ temp-system/coreutils/build.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="coreutils"
+PKG_VERSION="8.22"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Coreutils package contains utilities for showing and setting the basic system characteristics"
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "../../sources/$TARBALL" "$TARBALL"
+    source "${CONFIG_FILE}"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    cat > config.cache << EOF
+fu_cv_sys_stat_statfs2_bsize=yes
+gl_cv_func_working_mkstemp=yes
+EOF
+
+    patch -Np1 -i ../"${PKG_NAME}-${PKG_VERSION}-noman-1.patch"
+
+    ./configure --prefix="${HOST_TOOLS_DIR}"        \
+                --build="${HOST}"                   \
+                --host="${TARGET}"                  \
+                --enable-install-program=hostname   \
+                --cache-file=config.cache
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "${HOST_TOOLS_DIR}/bin/cat" ]; then
+    touch DONE
+fi
--- temp-system/init.sh
+++ temp-system/init.sh
@@ -9,7 +9,7 @@ source "${INSTALL_DIR}/functions.sh"
 source "${CONFIG_FILE}"
 
 if [ -f "${CROSS_COMPILE_TOOLS_DIR}/gcc-final/DONE" ]; then
-    _list=(build-variables gmp mpfr mpc isl cloog zlib binutils gcc ncurses bash bzip2 check)
+    _list=(build-variables gmp mpfr mpc isl cloog zlib binutils gcc ncurses bash bzip2 check coreutils)
 
     for i in ${_list[@]}; do
         case $i in
