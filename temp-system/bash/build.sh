e1ad9ce3be187b605d959e4a91cec744e3b3fb89
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-14 01:26:13 +0530

    Added bash package

---
+++ temp-system/bash/build.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="bash"
+PKG_VERSION="4.3"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+BUILD_DIR="${PKG_NAME}-build"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Bash package contains the Bourne-Again SHell."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "../../sources/$TARBALL" "$TARBALL"
+    source "${CONFIG_FILE}"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    patch -Np1 -i ../"${PKG_NAME}-${PKG_VERSION}-branch_update-5.patch"
+
+    cat > config.cache << "EOF"
+ac_cv_func_mmap_fixed_mapped=yes
+ac_cv_func_strcoll_works=yes
+ac_cv_func_working_mktime=yes
+bash_cv_func_sigsetjmp=present
+bash_cv_getcwd_malloc=yes
+bash_cv_job_control_missing=present
+bash_cv_printf_a_format=yes
+bash_cv_sys_named_pipes=present
+bash_cv_ulimit_maxfds=yes
+bash_cv_under_sys_siglist=yes
+bash_cv_unusable_rtsigs=no
+gt_cv_int_divbyzero_sigfpe=yes
+EOF
+
+    ./configure --prefix="${HOST_TOOLS_DIR}"    \
+                --build="${HOST}"               \
+                --host="${TARGET}"              \
+                --without-bash-malloc           \
+                --cache-file=config.cache
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "${HOST_TOOLS_DIR}/bin/bash" ]; then
+    touch DONE
+fi
