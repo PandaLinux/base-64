9616365a3275aa65117cb16c1ebe43a53c45889f
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-14 02:52:47 +0530

    Added file package

---
+++ temp-system/file/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="file"
+PKG_VERSION="5.19"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The File package contains a utility for determining the type of a given file or files."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "../../sources/$TARBALL" "$TARBALL"
+    source "${CONFIG_FILE}"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    ./configure --prefix="${HOST_TOOLS_DIR}" \
+                --build="${HOST}"            \
+                --host="${TARGET}"
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "${HOST_TOOLS_DIR}/bin/file" ]; then
+    touch DONE
+fi
