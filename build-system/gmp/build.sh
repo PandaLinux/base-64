0bdc39886115bbc3e6a20c661ee322a668f9fb69
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-15 17:35:37 +0530

    Added gmp package

---
+++ build-system/gmp/build.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="gmp"
+PKG_VERSION="6.0.0"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}a.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: GMP is a library for arithmetic on arbitrary precision integers, rational numbers, and"
+    echo -e "floating-point numbers."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    CC="gcc -isystem /usr/include"          \
+    CXX="g++ -isystem /usr/include"         \
+    LDFLAGS="-Wl,-rpath-link,/usr/lib:/lib" \
+    ./configure --prefix=/usr               \
+                --enable-cxx                \
+                --docdir=/usr/share/doc/gmp-6.0.0
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make "${MAKE_PARALLEL}" check
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/lib/libgmp.so" ]; then
+    touch DONE
+fi
--- build-system/init.sh
+++ build-system/init.sh
@@ -11,7 +11,8 @@ if [ -f "${TEMP_SYSTEM_DIR}/vim/DONE" ]; then
     echo norm 'export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin' >> "${CONFIG_FILE}"
     source "${CONFIG_FILE}"
 
-    _list=(virtual-kernel-fs prepare-env testsuite-tools perl-temp linux-headers man-pages glibc adjust-toolchain m4)
+    _list=(virtual-kernel-fs prepare-env testsuite-tools perl-temp linux-headers man-pages glibc adjust-toolchain m4 \
+           gmp)
 
     for i in ${_list[@]}; do
         case $i in
