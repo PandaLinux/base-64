b17c74fbb2e5e2a9c5a719ed34bd8a5d9d6b01e5
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-15 23:40:31 +0530

    Added attr package

---
+++ build-system/attr/build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="attr"
+PKG_VERSION="2.4.47"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.src.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: Attr is a library for getting and setting POSIX.1e (formerly POSIX 6) draft 15 capabilities."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make -j1 tests root-tests
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install install-dev install-lib
+
+    mv -v /usr/lib/libattr.so.* /lib
+    ln -sfv ../../lib/$(readlink /usr/lib/libattr.so) /usr/lib/libattr.so
+    chmod 755 -v /lib/libattr.so.1.1.0
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/lib/libattr.so" ]; then
+    touch DONE
+fi
