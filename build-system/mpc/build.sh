6f9edc87ba2646e0094f0f0190d2ecc04e6d11f4
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-15 17:43:25 +0530

    Added mpc package

---
+++ build-system/mpc/build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="mpc"
+PKG_VERSION="1.0.2"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: MPC is a C library for the arithmetic of complex numbers with arbitrarily high precision"
+    echo -e "and correct rounding of the result."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    CC="gcc -isystem /usr/include"          \
+    LDFLAGS="-Wl,-rpath-link,/usr/lib:/lib" \
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make "${MAKE_PARALLEL}" check
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/lib/libmpc.so" ]; then
+    touch DONE
+fi
