bfa8ec2f0da94f876c25034d9a896cb5cbd95f24
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-15 15:26:24 +0530

    Added Dejagnu package
    DejaGNU is a part of testsuite tools

---
+++ build-system/testsuite-tools/dejagnu/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="dejagnu"
+PKG_VERSION="1.5.1"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The DejaGNU package contains a framework for testing other programs."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    ./configure --prefix="${HOST_TOOLS_DIR}"
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "${HOST_TOOLS_DIR}/bin/runtest" ]; then
+    touch DONE
+fi
