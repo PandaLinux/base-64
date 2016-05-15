16ab515e16cd45b9319997b10fec158523d9dd40
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-15 17:46:48 +0530

    Added isl package

--- build-system/init.sh
+++ build-system/init.sh
@@ -12,7 +12,7 @@ if [ -f "${TEMP_SYSTEM_DIR}/vim/DONE" ]; then
     source "${CONFIG_FILE}"
 
     _list=(virtual-kernel-fs prepare-env testsuite-tools perl-temp linux-headers man-pages glibc adjust-toolchain m4 \
-           gmp mpfr mpc)
+           gmp mpfr mpc isl)
 
     for i in ${_list[@]}; do
         case $i in
---
+++ build-system/isl/build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="isl"
+PKG_VERSION="0.12.2"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.lzma"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: ISL is a library for manipulating sets and relations of integer points bounded by linear"
+    echo -e "constraints."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    CC="gcc -isystem /usr/include" \
+    LDFLAGS="-Wl,-rpath-link,/usr/lib:/lib" \
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make "${MAKE_PARALLEL}" check
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+    mkdir -pv /usr/share/gdb/auto-load/usr/lib
+    mv -v /usr/lib/libisl*gdb.py /usr/share/gdb/auto-load/usr/lib
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/lib/libisl.so" ]; then
+    touch DONE
+fi
