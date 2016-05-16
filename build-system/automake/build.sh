9e11fc058e9a0c581167120a3295d7156b693007
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-16 17:40:39 +0530

    Added automake package

---
+++ build-system/automake/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="automake"
+PKG_VERSION="1.14.1"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Automake package contains programs for generating Makefiles for use with Autoconf."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make "${MAKE_PARALLEL}" check
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/bin/automake" ]; then
+    touch DONE
+fi
