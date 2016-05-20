0791fbb3557e24f155791f04e9af3cb0f5089a78
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-21 00:17:05 +0530

    Update
    Prettification for system checking

--- version-check.sh
+++ version-check.sh
@@ -5,32 +5,35 @@ source functions.sh
 
 # TODO: Prettify the version check for better readability
 # Simple script to list version numbers of critical development tools
-bash --version | head -n1 | cut -d" " -f2-4
-ld --version | head -n1 | cut -d" " -f3-
-bison --version | head -n1
-bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-
-chown --version | head -n1 | cut -d")" -f2
-diff --version | head -n1
-find --version | head -n1
-gawk --version | head -n1
-gcc --version | head -n1
-g++ --version | head -n1
-ldd $(which ${SHELL}) | grep libc.so | cut -d ' ' -f 3 | ${SHELL} | head -n 1 | cut -d ' ' -f 1-7
-grep --version | head -n1
-gzip --version | head -n1
-make --version | head -n1
-tic -V
-patch --version | head -n1
-sed --version | head -n1
-tar --version | head -n1
-makeinfo --version | head -n1
-xz --version | head -n1
 
-echo 'main(){}' | gcc -v -o /dev/null -x c - > dummy.log 2>&1
+echo warn "Checking system..."
+echo empty
+echo warn "Bash:        ${BOLD}$(bash --version | head -n1 | cut -d" " -f2-4)${NORM}"
+echo warn "Binutils:    ${BOLD}$(ld --version | head -n1 | cut -d" " -f3-)${NORM}"
+echo warn "Bison:       ${BOLD}$(bison --version | head -n1)${NORM}"
+echo warn "Bzip2:       ${BOLD}$(bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-)${NORM}"
+echo warn "Coreutils:   ${BOLD}$(chown --version | head -n1 | cut -d")" -f2)${NORM}"
+echo warn "Diffutils:   ${BOLD}$(diff --version | head -n1)${NORM}"
+echo warn "Findutils:   ${BOLD}$(find --version | head -n1)${NORM}"
+echo warn "Gawk:        ${BOLD}$(gawk --version | head -n1)${NORM}"
+echo warn "GCC:         ${BOLD}$(gcc --version | head -n1)${NORM}"
+echo warn "G++:         ${BOLD}$(g++ --version | head -n1)${NORM}"
+echo warn "Glibc:       ${BOLD}$(ldd $(which ${SHELL}) | grep libc.so | cut -d ' ' -f 3 | ${SHELL} | head -n 1 | cut -d ' ' -f 1-7)${NORM}"
+echo warn "Grep:        ${BOLD}$(grep --version | head -n1)${NORM}"
+echo warn "Gzip:        ${BOLD}$(gzip --version | head -n1)${NORM}"
+echo warn "Make:        ${BOLD}$(make --version | head -n1)${NORM}"
+echo warn "Ncurses:     ${BOLD}$(tic -V)${NORM}"
+echo warn "Patch:       ${BOLD}$(patch --version | head -n1)${NORM}"
+echo warn "Sed:         ${BOLD}$(sed --version | head -n1)${NORM}"
+echo warn "Tar:         ${BOLD}$(tar --version | head -n1)${NORM}"
+echo warn "Makeinfo:    ${BOLD}$(makeinfo --version | head -n1)${NORM}"
+echo warn "Xz-utils:    ${BOLD}$(xz --version | head -n1)${NORM}"
+
+echo 'int main(){}' | gcc -v -o /dev/null -x c - > dummy.log 2>&1
 
 if ! grep -q ' error' dummy.log; then
-  echo success "Compilation successful" && rm dummy.log
+    echo success "Compilation successful" && rm dummy.log
 else
-  echo error "Compilation FAILED - more development packages may need to be \
-installed. If you like, you can also view dummy.log for more details."
+    echo error "Compilation FAILED. If you like, you can also view dummy.log for more details."
+    exit 1
 fi
