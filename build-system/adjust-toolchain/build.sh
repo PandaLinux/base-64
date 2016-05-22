b612e65fa35beddbba7061483beff9dff9916053
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-23 02:04:49 +0530

    Fixed #3

--- build-system/adjust-toolchain/build.sh
+++ build-system/adjust-toolchain/build.sh
@@ -8,7 +8,7 @@ function build() {
     # Adjust GCC's specs so that they point to the new dynamic linker
     gcc -dumpspecs | \
     perl -p -e "s@${HOST_TOOLS_DIR}/lib/ld@/lib/ld@g;" \
-         -e "s@\*startfile_prefix_spec:\n@$_/usr/lib/ @g;" > \
+         -e 's@\*startfile_prefix_spec:\n@$_/usr/lib/ @g;' > \
          $(dirname $(gcc --print-libgcc-file-name))/specs
 }
 
@@ -17,7 +17,6 @@ time { build; }
 # Verify installation
 echo 'main(){}' > dummy.c
 gcc dummy.c
-
-if [ $(readelf -l a.out | grep ': /lib') ]; then
+if [ "$(readelf -l a.out | grep ': /lib')" ]; then
     touch DONE
 fi
