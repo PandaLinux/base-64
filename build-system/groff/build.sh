936f00f5e83be4dde17592e74a7404a72587ce55
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-23 19:46:04 +0530

    Added groff package

---
+++ build-system/groff/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="groff"
+PKG_VERSION="1.22.2"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Groff package contains programs for processing and formatting text."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    PAGE=A4                     \
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/bin/groff" ]; then
+    touch DONE
+fi
--- build-system/init.sh
+++ build-system/init.sh
@@ -15,7 +15,7 @@ if [ -f "${TEMP_SYSTEM_DIR}/vim/DONE" ]; then
     _list=(virtual-kernel-fs prepare-env testsuite-tools perl-temp linux-headers man-pages glibc adjust-toolchain m4 \
            gmp mpfr mpc isl cloog zlib flex bison flex binutils gcc attr acl sed pkg-config-lite ncurses shadow \
            util-linux procps-ng e2fsprogs libcap coreutils acl iana-etc libtool iproute2 bzip2 gdbm perl readline \
-           autoconf automake bash bc diffutils file gawk findutils gettext gperf grep)
+           autoconf automake bash bc diffutils file gawk findutils gettext gperf grep groff)
 
     for i in ${_list[@]}; do
         case $i in
