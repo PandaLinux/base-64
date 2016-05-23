9b89d965a3a6e14099b305454603673fbf98ca18
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-23 22:44:58 +0530

    Added psmisc package

---
+++ build-system/psmisc/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="psmisc"
+PKG_VERSION="22.21"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Psmisc package contains programs for displaying information about running processes."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    echo ""
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/bin/fuser" ]; then
+    touch DONE
+fi
