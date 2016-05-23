7081cbd3e30b28a4ff40ab244f3d2b80d4688526
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-23 22:13:18 +0530

    Added expat package

---
+++ build-system/expat/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set +h		# disable hashall
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="expat"
+PKG_VERSION="2.1.0"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+BUILD_DIR="${PKG_NAME}-build"
+
+function help() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: Expat is a stream-oriented XML parser library written in C."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv "/sources/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf "${TARBALL}"
+}
+
+function build() {
+    ./configure --prefix=/usr
+
+    make "${MAKE_PARALLEL}"
+}
+
+function test() {
+    make "${MAKE_PARALLEL}" check
+}
+
+function instal() {
+    make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "${TARBALL}"
+}
+
+# Run the installation procedure
+time { help;clean;prepare;unpack;pushd "${SRC_DIR}";build;[[ "${MAKE_TESTS}" = TRUE ]] && test;instal;popd;clean; }
+# Verify installation
+if [ -f "/usr/bin/xmlwf" ]; then
+    touch DONE
+fi
