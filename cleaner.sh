1300d64e0e2803db41c74bacc39edbfc18216da0
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-25 19:25:47 +0530

    Added cleaner.sh script
    This script removes all the repository files and folders

---
+++ cleaner.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source variables.sh
+source functions.sh
+
+# Path of current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Remove the installation folders
+echo warn "Removing installation files/folders..."
+requireRoot rm -rf "${DIR}/build-system" "${DIR}/configure-system" "${DIR}/cross-compile-tools" "${DIR}/docs"   \
+                   "${DIR}/finalize-system" "${DIR}/sources" "${DIR}/temp-system" "${DIR}/.git*" "${DIR}/*.md"  \
+                   "${DIR}/wget-list" "${DIR}/.config" "${HOST_TOOLS_DIR}" "${HOST_CROSS_TOOLS_DIR}" "${DIR}/*.sh"
+echo success "Finished!"
+echo empty
