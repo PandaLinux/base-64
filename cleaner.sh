dfd1eb0160c1f622b1c235d4a0c76ca2512834ea
Abhimanyu <desk.abhimanyu@gmail.com>
2016-05-28 02:48:09 +0530

    Update
    Added missed folders

--- cleaner.sh
+++ cleaner.sh
@@ -10,6 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo warn "Removing installation files/folders..."
 requireRoot rm -rf "${DIR}/build-system" "${DIR}/configure-system" "${DIR}/cross-compile-tools" "${DIR}/docs"   \
                    "${DIR}/finalize-system" "${DIR}/sources" "${DIR}/temp-system" "${DIR}/.git*" "${DIR}/*.md"  \
-                   "${DIR}/wget-list" "${DIR}/.config" "${HOST_TOOLS_DIR}" "${HOST_CROSS_TOOLS_DIR}" "${DIR}/*.sh"
+                   "${DIR}/wget-list" "${DIR}/.config" "${DIR}/tools" "${DIR}/cross-tools" "${HOST_TOOLS_DIR}"  \
+                   "${HOST_CROSS_TOOLS_DIR}" "${DIR}/*.sh"
 echo success "Finished!"
 echo empty
