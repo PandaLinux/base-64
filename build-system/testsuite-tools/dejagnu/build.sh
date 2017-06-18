91031d3b779ed56c118dc28cb64ad63e01707825
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2017-06-19 01:18:08 +0530

    Updated to v1.6

--- build-system/testsuite-tools/dejagnu/build.sh
+++ build-system/testsuite-tools/dejagnu/build.sh
@@ -4,7 +4,7 @@ shopt -s -o pipefail
 set -e 		# Exit on error
 
 PKG_NAME="dejagnu"
-PKG_VERSION="1.5.3"
+PKG_VERSION="1.6"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
