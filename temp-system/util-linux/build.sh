454c123a330506117bd67344e84247b3124ed6d3
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2017-06-19 01:02:52 +0530

    Updated to v2.29.2

--- temp-system/util-linux/build.sh
+++ temp-system/util-linux/build.sh
@@ -4,7 +4,7 @@ shopt -s -o pipefail
 set -e 		# Exit on error
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.25.2"
+PKG_VERSION="2.29.2"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
@@ -31,8 +31,12 @@ function build() {
                 --host=${TARGET}              \
                 --disable-makeinstall-chown   \
                 --disable-makeinstall-setuid  \
+				--disable-nologin			  \
                 --without-python
 
+	sed -i 's/-lncursesw -ltinfo/-lncurses/' Makefile
+	sed -i 's/LIBNCURSESW/LIBNCURSES/' config.h
+				
     make ${MAKE_PARALLEL}
 }
 
