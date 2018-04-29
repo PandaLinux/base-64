cb6c7370446cf0bbe0befe3998007f656741622e
cmladmin <cmladmin@panda.onemindservices.com>
2018-04-29 12:11:59 -0700

    Update

---
+++ temp-system/linux_headers/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+shopt -s -o pipefail
+set -e 		# Exit on error
+
+PKG_NAME="linux"
+PKG_VERSION="4.15.3"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function showHelp() {
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e "Description: The Linux Kernel contains a make target that installs “sanitized” kernel headers."
+    echo -e "--------------------------------------------------------------------------------------------------------------"
+    echo -e ""
+}
+
+function prepare() {
+    ln -sv ../../sources/${TARBALL} ${TARBALL}
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {
+    make ${MAKE_PARALLEL} distclean
+    make ${MAKE_PARALLEL} mrproper
+}
+
+function instal() {
+    make ${MAKE_PARALLEL} INSTALL_HDR_PATH=dest headers_install
+    cp -rv dest/include/* ${HOST_TDIR}/include
+}
+
+function clean() {
+    rm -rf ${SRC_DIR} ${TARBALL}
+}
+
+# Run the installation procedure
+time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;runTest;instal;popd;clean; }
+# Verify installation
+if [ -f /usr/include/asm/a.out.h ]; then
+    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
+fi
