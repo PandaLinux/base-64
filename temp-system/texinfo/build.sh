ed29d140a997e2a18a477fc148cca9b0ff792e81
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 14:17:11 -0700

    Updated texinfo to version 6.6

--- temp-system/texinfo/build.sh
+++ temp-system/texinfo/build.sh
@@ -4,10 +4,12 @@ shopt -s -o pipefail
 set -e 		# Exit on error
 
 PKG_NAME="texinfo"
-PKG_VERSION="6.3"
+PKG_VERSION="6.6"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+LINK="http://ftp.gnu.org/gnu/$PKG_NAME/$TARBALL"
 
 function showHelp() {
     echo -e "--------------------------------------------------------------------------------------------------------------"
@@ -17,33 +19,47 @@ function showHelp() {
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-    PERL=`which perl`                    \
-    ./configure --prefix=${HOST_TDIR}    \
-                --build=${HOST}          \
-                --host=${TARGET}
+  echo -e "Configuring $PKG_NAME"
 
-    make ${MAKE_PARALLEL}
+  ./configure --prefix=/tools
+  make "$MAKE_PARALLEL"
+}
+
+function verify() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" check
 }
 
 function instal() {
-    make ${MAKE_PARALLEL} install
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
-# Verify installation
-if [ -f ${TOOLS_DIR}/bin/info ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
-fi
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  verify
+  instal
+  popd
+  clean
+}
