5131602395294d704cce502c6b00c9cfe249b655
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 14:19:57 -0700

    Added xz version 5.2.4

---
+++ temp-system/xz/build.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+shopt -s -o pipefail
+set -e # Exit on error
+
+PKG_NAME="xz"
+PKG_VERSION="5.2.4"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+LINK="http://tukaani.org/$PKG_NAME/$TARBALL"
+
+function showHelp() {
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The XZ Utils package contains programs for compressing and decompressing files. Compressing text"
+  echo -e "files with XZ Utils yields a much better compression percentage than with the traditional gzip."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
+}
+
+function prepare() {
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
+}
+
+function unpack() {
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
+}
+
+function build() {
+  echo -e "Configuring $PKG_NAME"
+
+  ./configure --prefix=/tools
+  make "$MAKE_PARALLEL"
+}
+
+function verify() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" check
+}
+
+function instal() {
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
+}
+
+function clean() {
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
+}
+
+# Run the installation procedure
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  verify
+  instal
+  popd
+  clean
+}
