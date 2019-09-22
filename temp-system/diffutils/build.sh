db036d9cdb9ff612676d376aafc36153df13aaa2
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 13:38:29 -0700

    Updated Diffutils to version 3.7

--- temp-system/diffutils/build.sh
+++ temp-system/diffutils/build.sh
@@ -1,50 +1,64 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
-set -e 		# Exit on error
+set -e # Exit on error
 
 PKG_NAME="diffutils"
-PKG_VERSION="3.5"
+PKG_VERSION="3.7"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
 
+LINK="http://ftp.gnu.org/gnu/$PKG_NAME/$TARBALL"
+
 function showHelp() {
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e "Description: The Diffutils package contains programs that show the differences between files or directories."
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e ""
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The Diffutils package contains programs that show the differences between files or directories."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-	sed -i '233,237 s/max)/max) \\/' lib/intprops.h
+  echo -e "Configuring $PKG_NAME"
+  ./configure --prefix=/tools
+  make "$MAKE_PARALLEL"
+}
 
-    ./configure --prefix=${HOST_TDIR}    \
-                --build=${HOST}          \
-                --host=${TARGET}
-
-    make ${MAKE_PARALLEL}
+function verify() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" check
 }
 
 function instal() {
-    make ${MAKE_PARALLEL} install
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
-# Verify installation
-if [ -f ${TOOLS_DIR}/bin/diff ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
-fi
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  verify
+  instal
+  popd
+  clean
+}
