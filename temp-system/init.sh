d50acbf89d52173bf3e5b604f7417b9ab4195c81
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 13:58:05 -0700

    Updated Patch to version 2.7.6

--- temp-system/init.sh
+++ temp-system/init.sh
@@ -8,7 +8,7 @@ source "$SRC"/variables.sh
 echo warn "Constructing temporary system..."
 
 _list=(binutils_pass1 gcc_pass1 linux_headers glibc libstdc++ binutils_pass2 gcc_pass2 tcl expect dejagnu m4 ncurses
-  bash bison bzip2 coreutils diffutils file findutils gawk gettext grep gzip make)
+  bash bison bzip2 coreutils diffutils file findutils gawk gettext grep gzip make patch)
 
 for i in "${_list[@]}"; do
   case $i in
--- temp-system/patch/build.sh
+++ temp-system/patch/build.sh
@@ -1,49 +1,66 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
-set -e 		# Exit on error
+set -e # Exit on error
 
 PKG_NAME="patch"
-PKG_VERSION="2.7.5"
+PKG_VERSION="2.7.6"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
 
+LINK="http://ftp.gnu.org/gnu/$PKG_NAME/$TARBALL"
+
 function showHelp() {
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e "Description: The Patch package contains a program for modifying or creating files by applying a “patch” file"
-    echo -e "typically created by the diff program."
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e ""
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The Patch package contains a program for modifying or creating files by applying a “patch” file"
+  echo -e "typically created by the diff program."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-    ./configure --prefix=${HOST_TDIR}    \
-                --build=${HOST}          \
-                --host=${TARGET}
+  echo -e "Configuring $PKG_NAME"
 
-    make ${MAKE_PARALLEL}
+  ./configure --prefix=/tools
+  make "$MAKE_PARALLEL"
+}
+
+function verify() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" check
 }
 
 function instal() {
-    make ${MAKE_PARALLEL} install
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
-# Verify installation
-if [ -f ${TOOLS_DIR}/bin/patch ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
-fi
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  verify
+  instal
+  popd
+  clean
+}
