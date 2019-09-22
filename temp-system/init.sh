c915eeec2bfe3bacc158537bf84e718028992e5b
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 08:25:20 -0700

    Minor fixes to init.sh

--- temp-system/init.sh
+++ temp-system/init.sh
@@ -3,36 +3,34 @@
 shopt -s -o pipefail
 set -e 		# Exit on error
 
-source ${HOME}/variables.sh
-source ${HOME}/functions.sh
-
-verify-user;
+source "$SRC"/variables.sh
+source "$SRC"/functions.sh
 
 echo warn "Constructing temporary system..."
 
 # Create log directory
 LOGS_DIR_TEMP_SYSTEM=${LOGS_DIR}/temp-system
-install -d ${LOGS_DIR_TEMP_SYSTEM}
+install -d "${LOGS_DIR_TEMP_SYSTEM}"
 # Create DONE directory
 DONE_DIR_TEMP_SYSTEM=${DONE_DIR}/temp-system
-install -d ${DONE_DIR_TEMP_SYSTEM}
+install -d "${DONE_DIR_TEMP_SYSTEM}"
 
 export DONE_DIR_TEMP_SYSTEM
 
 _list=(binutils_pass1 gcc_pass1 linux_headers)
 
-    for i in ${_list[@]}; do
+    for i in "${_list[@]}"; do
         case $i in
             * )
-                pushd ${i}
-                    if [ -f ${DONE_DIR_TEMP_SYSTEM}/${i} ]; then
+                pushd "${i}"
+                    if [ -f "${DONE_DIR_TEMP_SYSTEM}"/${i} ]; then
                         echo success "${i} --> Already Built"
                     else
                         echo empty
                         echo warn "Building ---> ${i}"
-                        bash build.sh |& tee ${LOGS_DIR_TEMP_SYSTEM}/${i}.log
+                        bash build.sh |& tee "${LOGS_DIR_TEMP_SYSTEM}"/${i}.log
 
-                        if [ -f ${DONE_DIR_TEMP_SYSTEM}/${i} ]; then
+                        if [ -f "${DONE_DIR_TEMP_SYSTEM}"/${i} ]; then
                             echo success "Building ---> ${i} completed"
                         else
                             echo error "Building ---> ${i} failed"
