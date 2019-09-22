1dc56e0877460cf35afb6ae2d42b395b2377c2b0
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 13:21:25 -0700

    Updated Bzip2 to version 1.0.8

--- temp-system/bzip2/build.sh
+++ temp-system/bzip2/build.sh
@@ -1,47 +1,58 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
+set -e # Exit on error
 
 PKG_NAME="bzip2"
-PKG_VERSION="1.0.6"
+PKG_VERSION="1.0.8"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
 
+LINK="http://www.bzip.org/$PKG_VERSION/$TARBALL"
+
 function showHelp() {
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e "Description: The Bzip2 package contains programs for compressing and decompressing files. Compressing text"
-    echo -e "files with bzip2 yields a much better compression percentage than with the traditional gzip."
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e ""
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The Bzip2 package contains programs for compressing and decompressing files. Compressing text"
+  echo -e "files with bzip2 yields a much better compression percentage than with the traditional gzip."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-    cp -v Makefile{,.orig}
-    sed -e '/^all/s/ test$//' Makefile.orig > Makefile
-
-    make CC="${CC}" AR="${AR}" RANLIB="${RANLIB}"
+  echo -e "Configuring $PKG_NAME"
+  make "$MAKE_PARALLEL"
 }
 
 function instal() {
-    make PREFIX=${HOST_TDIR} install
+  echo -e "Installing $PKG_NAME"
+  make "$MAKE_PARALLEL" PREFIX=/tools install
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
-# Verify installation
-if [ -f ${TOOLS_DIR}/bin/bzip2 ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
-fi
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  instal
+  popd
+  clean
+}
