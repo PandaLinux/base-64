7a9345c19eb5e69a94efd57d1c5ad200654d1047
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 13:13:56 -0700

    Updated bash to version 5.0

--- temp-system/bash/build.sh
+++ temp-system/bash/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
-set -e 		# Exit on error
+set -e # Exit on error
 
 PKG_NAME="bash"
-PKG_VERSION="4.4"
+PKG_VERSION="5.0"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
@@ -12,59 +12,58 @@ SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
 PATCH=${PKG_NAME}-${PKG_VERSION}-branch_update-1.patch
 
 function showHelp() {
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e "Description: The Bash package contains the Bourne-Again SHell."
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e ""
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The Bash package contains the Bourne-Again SHell."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
-    ln -sv ../../patches/${PATCH} ${PATCH}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-    patch -Np1 -i ../${PATCH}
+  echo -e "Configuring $PKG_NAME"
+  ./configure --prefix=/tools --without-bash-malloc
+  make "$MAKE_PARALLEL"
+}
 
-    cat > config.cache << "EOF"
-ac_cv_func_mmap_fixed_mapped=yes
-ac_cv_func_strcoll_works=yes
-ac_cv_func_working_mktime=yes
-bash_cv_func_sigsetjmp=present
-bash_cv_getcwd_malloc=yes
-bash_cv_job_control_missing=present
-bash_cv_printf_a_format=yes
-bash_cv_sys_named_pipes=present
-bash_cv_ulimit_maxfds=yes
-bash_cv_under_sys_siglist=yes
-bash_cv_unusable_rtsigs=no
-gt_cv_int_divbyzero_sigfpe=yes
-EOF
-
-    ./configure --prefix=${HOST_TDIR}   \
-                --build=${HOST}         \
-                --host=${TARGET}        \
-                --without-bash-malloc   \
-                --cache-file=config.cache
-
-    make ${MAKE_PARALLEL}
+function tests() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" test
 }
 
 function instal() {
-    make ${MAKE_PARALLEL} install
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
+  ln -sv bash /tools/bin/sh
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL} ${PATCH}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  tests
+  instal
+  popd
+  clean
+}
 # Verify installation
 if [ -f ${TOOLS_DIR}/bin/bash ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
+  touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
 fi
--- temp-system/init.sh
+++ temp-system/init.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
-set -e 		# Exit on error
+set -e # Exit on error
 
 source "$SRC"/variables.sh
 
 echo warn "Constructing temporary system..."
 
-_list=(binutils_pass1 gcc_pass1 linux_headers glibc libstdc++ binutils_pass2 gcc_pass2 tcl expect dejagnu m4 ncurses)
+_list=(binutils_pass1 gcc_pass1 linux_headers glibc libstdc++ binutils_pass2 gcc_pass2 tcl expect dejagnu m4 ncurses
+  bash)
 
-    for i in "${_list[@]}"; do
-        case $i in
-            * )
-                pushd "${i}"
-                    echo empty
-                    echo warn "Building ---> ${i}"
-                    bash build.sh
-                    echo success "Finished ---> ${i}"
-                popd;;
-        esac
-    done
+for i in "${_list[@]}"; do
+  case $i in
+  *)
+    pushd "${i}"
+    echo empty
+    echo warn "Building ---> ${i}"
+    bash build.sh
+    echo success "Finished ---> ${i}"
+    popd
+    ;;
+  esac
+done
 echo empty
