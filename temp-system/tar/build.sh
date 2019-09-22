0a928f4d39d58965ad464698fbb73f92f71cb36b
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-22 14:15:47 -0700

    Updated Tar to version 1.32

--- temp-system/tar/build.sh
+++ temp-system/tar/build.sh
@@ -1,61 +1,65 @@
 #!/usr/bin/env bash
 
 shopt -s -o pipefail
-set -e 		# Exit on error
+set -e # Exit on error
 
 PKG_NAME="tar"
-PKG_VERSION="1.29"
+PKG_VERSION="1.32"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
 
+LINK="http://ftp.gnu.org/gnu/$PKG_NAME/$TARBALL"
+
 function showHelp() {
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e "Description: The Tar package contains an archiving program."
-    echo -e "--------------------------------------------------------------------------------------------------------------"
-    echo -e ""
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e "Description: The Tar package contains an archiving program."
+  echo -e "--------------------------------------------------------------------------------------------------------------"
+  echo -e ""
 }
 
 function prepare() {
-    ln -sv ../../sources/${TARBALL} ${TARBALL}
+  echo -e "Downloading $TARBALL from $LINK"
+  wget "$LINK" -O "$TARBALL"
 }
 
 function unpack() {
-    tar xf ${TARBALL}
+  echo -e "Unpacking $TARBALL"
+  tar xf ${TARBALL}
 }
 
 function build() {
-    cat > config.cache << EOF
-gl_cv_func_wcwidth_works=yes
-gl_cv_func_btowc_eof=yes
-ac_cv_func_malloc_0_nonnull=yes
-gl_cv_func_mbrtowc_incomplete_state=yes
-gl_cv_func_mbrtowc_nul_retval=yes
-gl_cv_func_mbrtowc_null_arg1=yes
-gl_cv_func_mbrtowc_null_arg2=yes
-gl_cv_func_mbrtowc_retval=yes
-gl_cv_func_wcrtomb_retval=yes
-EOF
+  echo -e "Configuring $PKG_NAME"
 
-    ./configure --prefix=${HOST_TDIR}    \
-                --build=${HOST}          \
-                --host=${TARGET}         \
-                --cache-file=config.cache
+  ./configure --prefix=/tools
+  make "$MAKE_PARALLEL"
+}
 
-    make ${MAKE_PARALLEL}
+function verify() {
+  echo -e "Running tests for $PKG_NAME"
+  make "$MAKE_PARALLEL" check
 }
 
 function instal() {
-    make ${MAKE_PARALLEL} install
+  echo -e "Installing $PKG_NAME"
+  make "${MAKE_PARALLEL}" install
 }
 
 function clean() {
-    rm -rf ${SRC_DIR} ${TARBALL}
+  echo -e "Cleaning up..."
+  rm -rf ${SRC_DIR} ${TARBALL}
 }
 
 # Run the installation procedure
-time { showHelp;clean;prepare;unpack;pushd ${SRC_DIR};build;instal;popd;clean; }
-# Verify installation
-if [ -f ${TOOLS_DIR}/bin/tar ]; then
-    touch ${DONE_DIR_TEMP_SYSTEM}/$(basename $(pwd))
-fi
+time {
+  showHelp
+  clean
+  prepare
+  unpack
+  pushd ${SRC_DIR}
+  build
+  verify
+  instal
+  popd
+  clean
+}
