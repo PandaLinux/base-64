e3cf537bf066fc07b705dc17ca195f60fd4038d7
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2019-09-24 04:41:30 -0700

    Refactored the code

--- functions.sh
+++ functions.sh
@@ -4,133 +4,130 @@
 
 # Override echo to colorize output
 echo() {
-    case $1 in
-        success )
-            command echo "${GREEN}$2${NORM}";;
+  case $1 in
+  success)
+    command echo "${GREEN}$2${NORM}"
+    ;;
 
-        warn )
-            command echo "${YELLOW}$2${NORM}";;
+  warn)
+    command echo "${YELLOW}$2${NORM}"
+    ;;
 
-        error )
-            command echo "${RED}$2${NORM}";;
+  error)
+    command echo "${RED}$2${NORM}"
+    ;;
 
-        bold )
-            command echo "${BOLD}$2${NORM}";;
+  bold)
+    command echo "${BOLD}$2${NORM}"
+    ;;
 
-        rev )
-            command echo "${REV}$2${NORM}";;
+  rev)
+    command echo "${REV}$2${NORM}"
+    ;;
 
-        norm )
-            command echo "$2";;
+  norm)
+    command echo "$2"
+    ;;
 
-        empty )
-            command echo "";;
-    esac
+  empty)
+    command echo ""
+    ;;
+  esac
 }
 
 # This script tries to run the command as `root`
 function requireRoot() {
-    # Already root?
-    if [[ $(whoami) == 'root' ]]; then
-        $*
-    elif [[ -x /bin/sudo || -x /usr/bin/sudo ]]; then
-        sudo "$*"
-    else
-        echo error "We require root privileges to install."
-        echo error "Please run the script as root."
-        echo empty
-        exit 1
-    fi
+  # Already root?
+  if [[ $(whoami) == 'root' ]]; then
+    $*
+  elif [[ -x /bin/sudo || -x /usr/bin/sudo ]]; then
+    sudo "$*"
+  else
+    echo error "We require root privileges to install."
+    echo error "Please run the script as root."
+    echo empty
+    exit 1
+  fi
 }
 
 # Override `pushd` to dump data onto `/dev/null`
 pushd() {
-    command pushd "$@" > /dev/null
+  command pushd "$@" >/dev/null
 }
 
 # Override `popd` to dump data onto `/dev/null`
 popd() {
-    command popd "$@" > /dev/null
+  command popd "$@" >/dev/null
 }
 
 # Verify command ended successfully
 function checkCommand() {
-	if [ "$?" -eq 0 ]; then
-		echo success "Finished!"
-	else
-		[ $# -gt 0 ] && echo error "$@" || echo error "Failed!"
-        exit 1
-	fi
+  if [ "$?" -eq 0 ]; then
+    echo success "Finished!"
+    echo empty
+  else
+    [ $# -gt 0 ] && echo error "$@" || echo error "Failed!"
+    exit 1
+  fi
 }
 
 # Override cp
 cp() {
-    command cp "$@"
-    checkCommand;
+  command cp "$@"
+  checkCommand
 }
 
 # Temporary chroot environment
 function chrootTmp() {
-    sudo mount --bind /dev      "${INSTALL_DIR}"/dev
-    sudo mount -t devpts devpts "${INSTALL_DIR}"/dev/pts
-    sudo mount -t proc proc     "${INSTALL_DIR}"/proc
-    sudo mount -t sysfs sysfs   "${INSTALL_DIR}"/sys
-    sudo mount -t tmpfs tmpfs   "${INSTALL_DIR}"/run
+  sudo mount --bind /dev "${INSTALL_DIR}"/dev
+  sudo mount -t devpts devpts "${INSTALL_DIR}"/dev/pts
+  sudo mount -t proc proc "${INSTALL_DIR}"/proc
+  sudo mount -t sysfs sysfs "${INSTALL_DIR}"/sys
+  sudo mount -t tmpfs tmpfs "${INSTALL_DIR}"/run
 
-    sudo chroot "${INSTALL_DIR}" /tools/bin/env -i  \
-    HOME=/root TERM="${TERM}" PS1='\u:\w\$ '              \
+  sudo chroot "${INSTALL_DIR}" /tools/bin/env -i \
+    HOME=/root TERM="${TERM}" PS1='\u:\w\$ ' \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
     /tools/bin/bash -c "$@" +h
 
-    sync && sleep 1
-    sudo umount -l "${INSTALL_DIR}"/{run,sys,proc,dev/pts,dev}
+  sync && sleep 1
+  sudo umount -l "${INSTALL_DIR}"/{run,sys,proc,dev/pts,dev}
 }
 
 # System chroot environment
 function chrootSys() {
-    sudo mount --bind /dev      "${INSTALL_DIR}"/dev
-    sudo mount -t devpts devpts "${INSTALL_DIR}"/dev/pts
-    sudo mount -t proc proc     "${INSTALL_DIR}"/proc
-    sudo mount -t sysfs sysfs   "${INSTALL_DIR}"/sys
-    sudo mount -t tmpfs tmpfs   "${INSTALL_DIR}"/run
+  sudo mount --bind /dev "${INSTALL_DIR}"/dev
+  sudo mount -t devpts devpts "${INSTALL_DIR}"/dev/pts
+  sudo mount -t proc proc "${INSTALL_DIR}"/proc
+  sudo mount -t sysfs sysfs "${INSTALL_DIR}"/sys
+  sudo mount -t tmpfs tmpfs "${INSTALL_DIR}"/run
 
-    sudo chroot "${INSTALL_DIR}" /usr/bin/env -i  \
-    HOME=/root TERM="${TERM}" PS1='\u:\w\$ '      \
-    PATH=/bin:/usr/bin:/sbin:/usr/sbin          \
+  sudo chroot "${INSTALL_DIR}" /usr/bin/env -i \
+    HOME=/root TERM="${TERM}" PS1='\u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin \
     /bin/bash -c "$@" +h
 
-    sync && sleep 1
-    sudo umount -l "${INSTALL_DIR}"/{run,sys,proc,dev/pts,dev}
-}
-
-# Ask for confirmation to begin
-function askConfirm() {
-    read -p "${YELLOW}Continue? [Y/n]:${NORM} " -n 1 -r
-    echo empty
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        exit 0
-    fi
-    echo empty
+  sync && sleep 1
+  sudo umount -l "${INSTALL_DIR}"/{run,sys,proc,dev/pts,dev}
 }
 
 function testBootOrChroot() {
-	# Continue if building on and for the same arch
-	if [ "$(uname -m)" != 'x86_64' ]; then
-		echo empty
-		echo warn "Testing the system!"
-		/tools/lib/libc.so.6 > /dev/null && /tools/bin/gcc --version > /dev/null
-		checkCommand "Please submit this issue on our Github account!";
-		# TODO: Add the script to prepare the system for booting procedure
-	fi
+  # Continue if building on and for the same arch
+  if [ "$(uname -m)" != 'x86_64' ]; then
+    echo empty
+    echo warn "Testing the system!"
+    /tools/lib/libc.so.6 >/dev/null && /tools/bin/gcc --version >/dev/null
+    checkCommand "Please submit this issue on our Github account!"
+  fi
 }
 
 # Cleans up the newly created system
 function cleanup() {
-	echo empty
-	echo warn "Cleaning the system..."
+  echo empty
+  echo warn "Cleaning the system..."
 
-	requireRoot rm -rf "${INSTALL_DIR}"/{build-system,configure-system,cross-compile-tools,docs,finalize-system,patches,sources,temp-system}
-	requireRoot rm -rf "${INSTALL_DIR}"/{*.md,*.git*,*.sh,wget-list,md5sums}
-	requireRoot rm -rf "${INSTALL_DIR}"/tools
-	checkCommand;
+  requireRoot rm -rf "${INSTALL_DIR}"/{build-system,configure-system,cross-compile-tools,docs,finalize-system,patches,sources,temp-system}
+  requireRoot rm -rf "${INSTALL_DIR}"/{*.md,*.git*,*.sh,wget-list,md5sums}
+  requireRoot rm -rf "${INSTALL_DIR}"/tools
+  checkCommand
 }
--- install.sh
+++ install.sh
@@ -12,106 +12,17 @@ source "$SRC"/functions.sh
 
 # Display help messgae
 function show_help() {
-    cat << EOF
-Usage: ${0##*/} [-i INSTALL_DIR] [-j CONCURRENT JOBS] [-t RUN TESTS] ...
+  cat <<EOF
+Usage: ${0##*/} [temp-system|basic-system|configure-system|finalize-system]
 
 Compile & install 64bit Panda Linux
-
-Note: If you decide not to use any arguments, the previously set values will
-be used by default.
-
-    -b          Whether to create backup. Defaults to FALSE.
-                    TRUE    - Create
-                    FALSE   - Skip
-
-    -h          Display this help and exit
-
-    -i          Sets the installation directory.
-
-    -j          Run concurrent jobs. Defaults to value in nproc.
-                    0       - Uses all cores
-                    1       - Any number onwards 1 no. of core(s) will be used
-
-    -r          Resets options to their default value
-
-    -t          Whether to run all the tests. Defaults to TRUE.
-                    TRUE    - Run
-                    FALSE   - Don't run
-
 EOF
 }
-
-# Parse options
-while getopts ":t:j:i:hb:r:" opt; do
-    case ${opt} in
-
-        b )
-            if [ "${OPTARG}" = TRUE ] || [ "${OPTARG}" = FALSE ]; then
-                sed -i "s#.*DO_BACKUP=.*#DO_BACKUP=${OPTARG}#" variables.sh
-            else
-                echo error "Invalid argument. -b only takes either 'TRUE' or 'FALSE'."
-                exit 1
-            fi
-            ;;
-
-        h )
-            show_help;
-            exit 1
-            ;;
-
-        i )
-            # Validate path provided by the user
-            # Make sure a filesystem is mounted on this provided path
-            sed -i "s#.*INSTALL_DIR=.*#INSTALL_DIR=${OPTARG}#" variables.sh
-            sed -i "s#.*DONE_DIR=.*#DONE_DIR=${OPTARG}/done#" variables.sh
-            sed -i "s#.*LOGS_DIR=.*#LOGS_DIR=${OPTARG}/logs#" variables.sh
-            ;;
-
-        j )
-            if [ "${OPTARG}" -eq 0 ]; then
-                sed -i "s#.*MAKE_PARALLEL=.*#MAKE_PARALLEL=-j$(cat /proc/cpuinfo | grep -c processor)#" variables.sh
-            else
-                sed -i "s#.*MAKE_PARALLEL=.*#MAKE_PARALLEL=-j${OPTARG}#" variables.sh
-            fi
-            ;;
-
-        r )
-            sed -i "s#.*DO_BACKUP=.*#DO_BACKUP=TRUE#" variables.sh
-            sed -i "s#.*INSTALL_DIR=.*#INSTALL_DIR=/tmp/panda64#" variables.sh
-            sed -i "s#.*MAKE_PARALLEL=.*#MAKE_PARALLEL=-j$(cat /proc/cpuinfo | grep -c processor)#" variables.sh
-            sed -i "s#.*MAKE_TESTS=.*#MAKE_TESTS=TRUE#" variables.sh
-            sed -i "s#.*DONE_DIR=.*#DONE_DIR=/tmp/panda64/done#" variables.sh
-            sed -i "s#.*LOGS_DIR=.*#LOGS_DIR=/tmp/panda64/logs#" variables.sh
-            ;;
-
-        t )
-            if [ "${OPTARG}" = TRUE ] || [ "${OPTARG}" = FALSE ]; then
-                sed -i "s#.*MAKE_TESTS=.*#MAKE_TESTS=${OPTARG}#" variables.sh
-            else
-                echo error "Invalid argument. -t only takes either 'TRUE' or 'FALSE'."
-                exit 1
-            fi
-            ;;
-
-        \? )
-            echo error "Invalid option: -{$OPTARG}" >&2
-            exit 1
-            ;;
-
-        : )
-            echo error "Option -${OPTARG} requires an argument."
-            exit 1
-            ;;
-    esac
-done
-
-source "${SRC}"/variables.sh
 
 # Show installation configuration information to the user
 echo empty
 echo warn "General Installation Configuration"
 echo norm "${BOLD}Installation Directory:${NORM}    ${INSTALL_DIR}"
-echo norm "${BOLD}Do backup:${NORM}                 ${DO_BACKUP}"
 echo norm "${BOLD}Run tests:${NORM}                 ${MAKE_TESTS}"
 echo norm "${BOLD}No. of jobs:${NORM}               ${MAKE_PARALLEL}"
 echo norm "${BOLD}Target:${NORM}                    ${TARGET}"
@@ -119,64 +30,61 @@ echo norm "${BOLD}Path:${NORM}                      ${PATH}"
 echo empty
 echo norm "${BOLD}Tools Directory:${NORM}           /tools"
 echo empty
-echo norm "${BOLD}Done Directory:${NORM}            ${DONE_DIR}"
-echo norm "${BOLD}Logs Directory:${NORM}            ${LOGS_DIR}"
-echo empty
 
 # Validate path provided by the user
 if [ ! -d "$INSTALL_DIR" ]; then
+  echo warn "Creating $INSTALL_DIR"
   requireRoot mkdir -p "$INSTALL_DIR"
-fi
-
-if [ ! -d "${INSTALL_DIR}"/dev ]; then
-    # Get ${INSTALL_DIR} permissions
-    requireRoot chown -R $(whoami) "${INSTALL_DIR}"
-    # Create necessary directories and symlinks
-    echo warn "Creating necessary folders..."
-    
-    if [ $(readlink /tools) ]; then
-        requireRoot rm -rf /tools
-    fi
-    
-    install -d "${INSTALL_DIR}"/tools
-    install -d "${LOGS_DIR}"
-    install -d "${DONE_DIR}"
-
-    requireRoot ln -s "${INSTALL_DIR}"/tools /
 fi
 
 #----------------------------------------------------------------------------------------------------#
 #                               S T A R T   I N S T A L L A T I O N                                  #
 #----------------------------------------------------------------------------------------------------#
 
-if [ ! -f "${INSTALL_DIR}"/.done ]; then
-	echo empty
-	echo success "Starting installation..."
-	echo empty
+echo empty
+echo success "Starting installation..."
 
-	# Copying data to the installation location
-	echo warn "Copying data to ${INSTALL_DIR}. Please wait..."
-	cp -ur ./* "${INSTALL_DIR}"
+# Copying data to the installation location
+echo warn "Copying data to ${INSTALL_DIR}. Please wait..."
+cp -ur ./* "${INSTALL_DIR}"
 
-	# Constructing temporary system
-	pushd "${TEMP_SYSTEM_DIR}" && bash init.sh && popd
+case "$1" in
+temp-system)
+  # Constructing temporary system
+  pushd "${TEMP_SYSTEM_DIR}" && bash init.sh && popd
+  ;;
 
-	# Test to boot or to chroot
-	testBootOrChroot;
+testChroot)
+  testBootOrChroot
+  ;;
 
-	# Building the actual system
-	pushd "${BUILD_SYSTEM_DIR}" && bash init.sh && popd
+basic-system)
+  # Building the basic system
+  pushd "${BUILD_SYSTEM_DIR}" && bash init.sh && popd
+  ;;
 
-	# Configuring the system
-	pushd "${CONFIGURE_SYSTEM_DIR}" && bash init.sh && popd
+configure-system)
+  # Configuring the system
+  pushd "${CONFIGURE_SYSTEM_DIR}" && bash init.sh && popd
+  ;;
 
-	# Finalize the system
-	pushd "${FINALIZE_SYSTEM_DIR}" && bash init.sh && popd
+finalize-system)
+  # Finalize the system
+  pushd "${FINALIZE_SYSTEM_DIR}" && bash init.sh && popd
+  ;;
 
-else
-	echo success "Installation Finished!"
-	echo empty
-fi
+*)
+  echo error "Invalid option selected! Exiting..."
+  exit 1
+  ;;
 
-# Creates backup of the system if -b is TRUE
-createBackup;
+esac
+
+#----------------------------------------------------------------------------------------------------#
+#                               F I N I S H   I N S T A L L A T I O N                                #
+#----------------------------------------------------------------------------------------------------#
+
+echo empty
+echo warn "Removing installation files"
+rm -rf "$SRC"
+echo success "Done!"
